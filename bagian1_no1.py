b98a776a2e24e5c2d612e7848432068b79403232
Aisyahen <aisyahnurhidayah113@gmail.com>
2025-04-03 10:39:11 +0700

    Update bagian1_no1.py

--- bagian1_no1.py
+++ bagian1_no1.py
@@ -5,5 +5,6 @@ print("List sebelum operasi:", my_list)
 
 my_list.pop(0)  # Menghapus elemen pertama
 my_list.pop()   # Menghapus elemen terakhir
+my_list.insert(2, 25)  # Menyisipkan angka 25 pada indeks ke 2
 
 print("List setelah operasi:", my_list)
